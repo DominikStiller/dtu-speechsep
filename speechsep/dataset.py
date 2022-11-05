e032a676d1cd16702d3d6cbedfc820b868ea40c6
Dominik Stiller <domi.stiller@gmail.com>
2022-11-05 03:31:44 +0100

    Add LibriMix mini dataset

--- speechsep/dataset.py
+++ speechsep/dataset.py
@@ -1,5 +1,55 @@
 import os
 
+import numpy as np
+import pandas as pd
+import torch
+import torchaudio
+from torch.utils.data import Dataset
+
+from speechsep.model import valid_n_samples
+from speechsep.util import pad
+
+
+class LibrimixDataset(Dataset):
+    def __init__(self, metadata_file: str, example_length=8, pad_to_valid=False):
+        self.metadata = pd.read_csv(metadata_file)
+        self.pad_to_valid = pad_to_valid
+
+        if len(self) > 0:
+            _, self.sample_rate = torchaudio.load(self.metadata.iloc[0]["mixture_path"])
+        else:
+            raise "Empty dataset for LibriMix"
+
+        if self.metadata["length"].min() / self.sample_rate < example_length:
+            raise "Shortest length below given example_length"
+
+        self.n_samples = example_length * self.sample_rate
+        if pad_to_valid:
+            self.n_samples_valid = valid_n_samples(self.n_samples)
+        else:
+            self.n_samples_valid = self.n_samples
+        self.ts = np.arange(0, self.n_samples_valid / self.sample_rate, 1 / self.sample_rate)
+
+    def __len__(self):
+        return len(self.metadata.index)
+
+    def __getitem__(self, idx):
+        row = self.metadata.iloc[idx]
+
+        speaker1, _ = torchaudio.load(row["source_1_path"])
+        speaker2, _ = torchaudio.load(row["source_2_path"])
+        # Concatenate speaker signals
+        y_true = torch.cat([speaker1, speaker2], dim=0)[..., : self.n_samples]
+
+        x, _ = torchaudio.load(row["mixture_path"])
+        x = x[..., : self.n_samples]
+
+        if self.pad_to_valid:
+            x = pad(x, self.n_samples_valid)
+            y_true = pad(y_true, self.n_samples_valid)
+
+        return x, y_true
+
 
 def get_storage_dir():
     storage_dir = os.getenv("LIBRIMIX_STORAGE_DIR")
@@ -11,3 +61,19 @@ def get_storage_dir():
 def get_audio_path(example: str, dataset: str):
     storage_dir = get_storage_dir()
     return f"{storage_dir}/Libri2Mix/wav8k/min/{dataset}/mix_both/{example}.wav"
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    dataset = LibrimixDataset("data/datasets/mini/mixture_mini_mix_both.csv", pad_to_valid=True)
+
+    x, y = dataset[0]
+    print(x.shape)
+    print(y.shape)
+    print(dataset.sample_rate)
+
+    plt.plot(dataset.ts, x[0])
+    plt.plot(dataset.ts, y[0])
+    plt.plot(dataset.ts, y[1])
+    plt.show()
--- speechsep/util.py
+++ speechsep/util.py
@@ -1,4 +1,8 @@
+from typing import Union
+
+import numpy as np
 import torch
+import torch.nn.functional as F
 
 
 def center_trim(to_trim: torch.Tensor, target: torch.Tensor, dim=-1):
@@ -14,3 +18,14 @@ def center_trim(to_trim: torch.Tensor, target: torch.Tensor, dim=-1):
         The trimmed to_trim tensor
     """
     return to_trim.narrow(dim, (to_trim.shape[dim] - target.shape[dim]) // 2, target.shape[dim])
+
+
+def pad(to_pad: Union[np.ndarray, torch.Tensor], target_length: int):
+    delta = int(target_length - to_pad.shape[-1])
+    padding_left = max(0, delta) // 2
+    padding_right = delta - padding_left
+
+    if isinstance(to_pad, np.ndarray):
+        return np.pad(to_pad, (padding_left, padding_right))
+    elif isinstance(to_pad, torch.Tensor):
+        return F.pad(to_pad, (padding_left, padding_right))
