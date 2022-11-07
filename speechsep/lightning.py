22ca66cac8b3def1f917d0b069b69bfcc9eb364c
kristian-267 <ufsi_kristianruud@protonmail.com>
2022-11-07 16:26:03 +0100

    implement stoi

--- speechsep/lightning.py
+++ speechsep/lightning.py
@@ -9,6 +9,7 @@ from speechsep.plotting import plot_separated_with_truth
 from speechsep.util import center_trim, save_as_audio
 
 from torchmetrics.audio.pesq import PerceptualEvaluationSpeechQuality
+from torchmetrics.audio.stoi import ShortTimeObjectiveIntelligibility
 
 
 class LitDemucs(pl.LightningModule):
@@ -28,14 +29,14 @@ class LitDemucs(pl.LightningModule):
         return loss
 
     def validation_step(self, batch, batch_idx):
-        loss, pesq, pesq0, pesq1 = self._shared_eval_step(batch, batch_idx)
-        metrics = {"val_loss": loss, "val_pesq": pesq, "val_pesq_0": pesq0, "val_pesq_1": pesq1}
+        loss, pesq, stoi = self._shared_eval_step(batch, batch_idx)
+        metrics = {"val_loss": loss, "val_pesq": pesq, "val_stoi": stoi}
         self.log_dict(metrics, on_step=False, on_epoch=True, prog_bar=True)
         return metrics
 
     def test_step(self, batch, batch_idx):
-        loss, pesq, pesq0, pesq1 = self._shared_eval_step(batch, batch_idx)
-        metrics = {"test_loss": loss, "test_pesq": pesq, "test_pesq_0": pesq0, "test_pesq_1": pesq1}
+        loss, pesq, stoi = self._shared_eval_step(batch, batch_idx)
+        metrics = {"test_loss": loss, "test_pesq": pesq, "test_stoi": stoi}
         self.log_dict(metrics)
         return metrics
 
@@ -44,12 +45,14 @@ class LitDemucs(pl.LightningModule):
         y_pred = self(x)
         y = center_trim(y, target=y_pred)
         loss = F.l1_loss(y_pred, y)
-        # calculate pesq
+        # calculate pesq and stoi
         nb_pesq = PerceptualEvaluationSpeechQuality(8000, 'nb')
-        pesq = nb_pesq(y_pred, y)
-        pesq0 = nb_pesq(y_pred[:, 0:1, :], y[:, 0:1, :])
-        pesq1 = nb_pesq(y_pred[:, 1:2, :], y[:, 1:2, :])
-        return loss, pesq, pesq0, pesq1
+        pesq = nb_pesq(y_pred.sum(1), y.sum(1))
+        # pesq0 = nb_pesq(y_pred[:, 0:1, :], y[:, 0:1, :])
+        # pesq1 = nb_pesq(y_pred[:, 1:2, :], y[:, 1:2, :])
+        stoi = ShortTimeObjectiveIntelligibility(8000, False)
+        stoi = stoi(y_pred.sum(1), y.sum(1))
+        return loss, pesq, stoi
 
     def configure_optimizers(self):
         optimizer = optim.Adam(self.parameters(), lr=3e-4)
